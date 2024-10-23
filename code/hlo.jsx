0a3cc35a8f4171f8ae161da4b14290fc497a0701
Ganesh-Jujjuru <hemasaiganesh2905@gmail.com>
2024-10-23 11:48:05 +0530

    Update hlo.jsx

--- code/hlo.jsx
+++ code/hlo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './style.css'; // Import the CSS file
+import './styles.css'; // Import the CSS file
 
 const App = () => {
   const [counters, setCounters] = useState([
